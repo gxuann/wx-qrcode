f69da60945b1d387d1dcd0b9e404a400b5938ddf
GuangXuan <tlfggx@gmail.com>
2018-02-09 00:22:31 +0800

    home&about

---
+++ dist/pages/home/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Page({
+  data: {
+    '__code__': {
+      readme: ''
+    }
+  },
+  toGenerate: function toGenerate(e) {
+    wx.navigateTo({
+      url: '../../pages/generate/index'
+    });
+  }
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4Lnd4cCJdLCJuYW1lcyI6WyJkYXRhIiwidG9HZW5lcmF0ZSIsImUiLCJ3eCIsIm5hdmlnYXRlVG8iLCJ1cmwiXSwibWFwcGluZ3MiOiI7Ozs7OztBQVFFQSxRQUFNO0FBQUE7QUFBQTtBQUFBO0FBQUEsRztBQUNOQyxjQUFXLG9CQUFTQyxDQUFULEVBQVc7QUFDcEJDLE9BQUdDLFVBQUgsQ0FBYztBQUNaQyxXQUFLO0FBRE8sS0FBZDtBQUdEIiwiZmlsZSI6ImluZGV4Lnd4cCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcbiAgY29uZmlnOiB7XG4gICAgbmF2aWdhdGlvbkJhclRpdGxlVGV4dDogJ1FS5LqM57u056CBJyxcbiAgICB1c2luZ0NvbXBvbmVudHM6IHtcbiAgICAgICd3eGMtZmxleCc6ICdAbWludWkvd3hjLWZsZXgnLFxuICAgICAgJ3d4Yy1pY29uJzogJ0BtaW51aS93eGMtaWNvbidcbiAgICB9XG4gIH0sXG4gIGRhdGE6IHt9LFxuICB0b0dlbmVyYXRlOmZ1bmN0aW9uKGUpe1xuICAgIHd4Lm5hdmlnYXRlVG8oe1xuICAgICAgdXJsOiAnLi4vLi4vcGFnZXMvZ2VuZXJhdGUvaW5kZXgnXG4gICAgfSlcbiAgfVxufSJdfQ==
